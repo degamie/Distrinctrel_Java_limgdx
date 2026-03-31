10b0e58d2c9fbcfbc83aa740616f6d0813100395
Degamie_sign <degamiesign@gmail.com>
2026-03-31 16:56:26 +0530

    App Build 2d WebHost#1

--- core/src/main/java/com/libgdx/discintrel/entity/AIAgent.java
+++ core/src/main/java/com/libgdx/discintrel/entity/AIAgent.java
@@ -1,4 +1,4 @@
-//Perceptron(AI(Agent)) entity Declare (31/03/2026)(GD)(Sarthak Mittal)#1.1.1,.1.1.1,1.1.1.1.1.1.1
+//Perceptron(AI(Agent)) entity Declare (31/03/2026)(GD)(Sarthak Mittal)#1.1.1,.1.1.1,1.1.1.1.1.1.1.1
 package com.libgdx.discintrel.entity;
 
 import com.badlogic.gdx.math.Rectangle;
@@ -7,6 +7,7 @@ import com.libgdx.discintrel.Service.DescintrelService;
 import org.w3c.dom.Text;
 
 public class AIAgent extends DisitrinctEntity{
+    public Text statsDisplay(Text statsDisplay){return statsDisplay;}
     public int getHeight(int height){return height;}
     public int getWidth(int width){return width;}//Fethcing Width in App
     public DescintrelService service;
