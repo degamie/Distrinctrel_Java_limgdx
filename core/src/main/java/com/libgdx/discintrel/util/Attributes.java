a9b29f767a463276a179ca6ace7afb3a3eaaefca
Degamie_sign <degamiesign@gmail.com>
2026-04-02 23:30:00 +0530

    Speed Commit #1.1_Fast

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,3 +1,4 @@
+//WID(2/4/2026)(Sarthak Mittal)(Degamiesign)#1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -15,12 +16,12 @@ public class Attributes {
     // Sprites & Textures
     public Texture squirrelTexture, nutTexture;
     public Sprite squirrelSprite, nutSprite;
-
     // Game Logic
     public float speed = 300.0f; // Pixels per second
     public int score = 0;
     public int level = 1;
     public boolean finishLineVisible = false;
+    public float getSpeed(float speed){return speed;}
 
     public BitmapFont font;
 }
