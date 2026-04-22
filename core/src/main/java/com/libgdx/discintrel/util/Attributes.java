2f1e21e7e6e07a7dc5924d3c5eff02670bd17e47
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-22 09:35:18 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(20/4/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1
+//WID(21/4/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Attributes {
+    public void setLevel(int level){this.level=level;}//binding level in App
     public Texture getBackgroundTexture(Texture texture){return texture;}
     public Texture backgroundTexture = new Texture(Gdx.files.internal("distrinctrel_background.jpeg"));;
     public SpriteBatch batch;
@@ -19,6 +20,7 @@ public class Attributes {
     public void setSquirrelTexture(Texture squirrelTexture){this.squirrelTexture=squirrelTexture;}
     public Texture squirrelTexture, nutTexture;
     public Sprite squirrelSprite, nutSprite;
+    public float getSpeed(float speed){}
     // Game Logic
     public float speed = 300.0f; // Pixels per second
     public int score = 0;
