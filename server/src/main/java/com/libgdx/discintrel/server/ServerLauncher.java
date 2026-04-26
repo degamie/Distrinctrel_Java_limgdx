e76ec11aeb520344ff2a12f143530b2d88816289
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-26 18:50:08 +0530

    Add files via upload

--- server/src/main/java/com/libgdx/discintrel/server/ServerLauncher.java
+++ server/src/main/java/com/libgdx/discintrel/server/ServerLauncher.java
@@ -1,8 +1,11 @@
+//WID(26/04/2026)(Sarthak Mittal)(DegamieSign)
 package com.libgdx.discintrel.server;
+
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 
 /** Launches the server application. */
 public class ServerLauncher {
     public static void main(String[] args) {
-        // TODO Implement server application.
+        HeadlessApplicationConfiguration headlessApplicationConfiguration=new HeadlessApplicationConfiguration();
     }
 }
