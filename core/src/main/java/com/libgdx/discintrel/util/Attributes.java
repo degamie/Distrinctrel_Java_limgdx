04cbe893c27beae4a51cc4fab6623836467b1178
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-26 09:32:27 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(21/4/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c
+//WID(26/4/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Attributes {
+    public void setBackgroundTexture(Texture texture){this.backgroundTexture=backgroundTexture;}//Bidning BackgroudnTexture in App
     public void setLevel(int level){this.level=level;}//binding level in App
     public Texture getBackgroundTexture(Texture texture){return texture;}
     public Texture backgroundTexture = new Texture(Gdx.files.internal("distrinctrel_background.jpeg"));;
@@ -20,7 +21,7 @@ public class Attributes {
     public void setSquirrelTexture(Texture squirrelTexture){this.squirrelTexture=squirrelTexture;}
     public Texture squirrelTexture, nutTexture;
     public Sprite squirrelSprite, nutSprite;
-    public float getSpeed(float speed){}
+    public void setSpeed(float speed){this.speed=speed;}//binding Speed in  App
     // Game Logic
     public float speed = 300.0f; // Pixels per second
     public int score = 0;
