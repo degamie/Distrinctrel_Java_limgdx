e5be8f25ffc51a2f3b258d4d48ae024d91777a8d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-02 06:18:03 +0530

    Add files via upload

---
+++ core/src/main/java/com/libgdx/discintrel/Repository/DistrinctrelRepository.java
@@ -0,0 +1,8 @@
+//WID(2/5/2026)(Sarthak Mittal)(DegamieSign)#1
+package com.libgdx.discintrel.Repository;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface DistrinctrelRepository {
+    public void setnutreset(SpriteBatch nutsprite);
+}
