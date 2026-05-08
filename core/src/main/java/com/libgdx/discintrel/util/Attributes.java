3536aab97750a4a74e1b0fa54203b64a1f0d8d2a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-08 09:41:36 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(06/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1
+//WID(08/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -9,6 +9,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Attributes {
+    public void existsByAttribtutes(Attributes attributes){
+        if(attributes!=null)getByAttributes(attributes);
+        else getByAttributes(null);
+    }
     public  Attributes getByAttributes(Attributes attributes) {
         return attributes;
     }
