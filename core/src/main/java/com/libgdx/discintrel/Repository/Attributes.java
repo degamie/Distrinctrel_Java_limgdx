8154f601dfa0622eaed4fa7214ac6b5d926732c4
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-08 23:44:14 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/Repository/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/Repository/Attributes.java
@@ -1,4 +1,4 @@
-//WID(29/4/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1
+//WID(08/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1.1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -9,6 +9,32 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Attributes {
+    public void updateByScore(int score){getscore(score)+setScore(score)+1;}
+
+    public int getscore(int score) {
+        return score;
+    }
+
+    public void existsByAttribtutes(Attributes attributes){
+        if(attributes!=null)getByAttributes(attributes);
+        else getByAttributes(null);
+    }
+    public  Attributes getByAttributes(Attributes attributes) {
+        return attributes;
+    }
+    public void updateByAttributes(Attributes attributes){getByAttributes(attributes)+setAttributes(attributes)+1;}
+
+
+
+    public void setAttributes(Attributes attributes){this.attributes=attributes;}//Binding Attributes in App
+    public Attributes attributes;
+    public void existsByspeed(int speed){
+        if(speed!=0)getSpeed(speed);
+        else getSpeed(0);
+    }
+    public void updateBySpeed(int speed){getSpeed(speed)+setSpeed(speed)+1;}//Updating Speed in App
+    public void setSpeed(int speed){this.speed=speed;}//binding Speed in App
+    public void setScore(int score){this.score=score;}//Binding Score in App
     public void setBatch(SpriteBatch batch){this.batch=batch;}
     public int getLevel(int level){return level;}//Fetching level in App
     public void setBackgroundTexture(Texture texture){this.backgroundTexture=backgroundTexture;}//Bidning BackgroudnTexture in App
@@ -16,6 +42,9 @@ public class Attributes {
     public Texture getBackgroundTexture(Texture texture){return texture;}
     public Texture backgroundTexture = new Texture(Gdx.files.internal("distrinctrel_background.jpeg"));;
     public SpriteBatch batch;
+    public void
+    public void setbatch(SpriteBatch batch){this.batch=batch;}//bidning Batch in App
+//    public SpriteBatch getBatch(SpriteBatch batch){return batch;}
     public OrthographicCamera camera;
 
     // Sprites & Textures
