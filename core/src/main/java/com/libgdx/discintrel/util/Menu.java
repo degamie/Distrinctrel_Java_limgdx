e93875ca18d089fa7f45d1f8cb6bfdefeeb2ef2f
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-09 13:06:30 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/util/Menu.java
+++ core/src/main/java/com/libgdx/discintrel/util/Menu.java
@@ -1,4 +1,4 @@
-//WID(08/05/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1/1.1/1.11.1.1.1.1/1.1/1
+//WID(09/05/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1/1.1/1.11.1.1.1.1/1.1/1/1
 package com.libgdx.discintrel.Menu;
 
 import com.badlogic.gdx.Gdx;
@@ -18,6 +18,7 @@ import com.libgdx.discintrel.Service.DescintrelService;
 import static com.badlogic.gdx.scenes.scene2d.utils.ScissorStack.getViewport;
 
 public class Menu implements Screen {
+    public Screen getScreen(Screen screen){return screen;}//Fetchign Screen in App
     public Screen screen;
     public void setscreen(Screen screen){this.screen=screen;}//binding Screen in App
 //    public void setMenu(Menu menu){this.menu=menu;}//Binding
