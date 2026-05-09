7c1f26c4222efa4fdcd4d4b43a0acd00312c2626
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-09 09:13:48 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(08/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1
+//WID(08/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1.1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -9,6 +9,12 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Attributes {
+    public void updateByScore(int score){getscore(score)+setScore(score)+1;}
+
+    public int getscore(int score) {
+        return score;
+    }
+
     public void existsByAttribtutes(Attributes attributes){
         if(attributes!=null)getByAttributes(attributes);
         else getByAttributes(null);
@@ -36,7 +42,7 @@ public class Attributes {
     public Texture getBackgroundTexture(Texture texture){return texture;}
     public Texture backgroundTexture = new Texture(Gdx.files.internal("distrinctrel_background.jpeg"));;
     public SpriteBatch batch;
-    public void
+//    public void
     public void setbatch(SpriteBatch batch){this.batch=batch;}//bidning Batch in App
 //    public SpriteBatch getBatch(SpriteBatch batch){return batch;}
     public OrthographicCamera camera;
