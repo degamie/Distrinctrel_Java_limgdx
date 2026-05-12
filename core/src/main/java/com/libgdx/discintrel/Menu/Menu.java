e0c7af99210e4b45eb409497ba53ee225fbe0634
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-12 21:17:23 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/Menu/Menu.java
+++ core/src/main/java/com/libgdx/discintrel/Menu/Menu.java
@@ -1,4 +1,4 @@
-//WID(10/05/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1/1.1/1.11.1.1.1.1/1.1/1/1.1  
+//WID(12/05/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1/1.1/1.11.1.1.1.1/1.1/1/1.1/1/1.1.1
 package com.libgdx.discintrel.Menu;
 
 import com.badlogic.gdx.Gdx;
@@ -18,6 +18,13 @@ import com.libgdx.discintrel.Service.DescintrelService;
 import static com.badlogic.gdx.scenes.scene2d.utils.ScissorStack.getViewport;
 
 public class Menu implements Screen {
+    public void updaatebyPlayBtn(TextButton playbtn){getPlaybtn(playbtn)+setPlaybtn(playbtn)+1;}//Updating playBtn in App
+    public void setPlaybtn(TextButton playbtn){this.playbtn=playbtn;}//binding playbtn in App
+    public void existsByScreen (Screen screen){
+        if(screen!=null)getScreen(screen);
+        else getScreen(null);
+    }
+    public void updateByScreen(Screen screen){getScreen(screen)+setScreen(screen)+1;}//Updating Screen in  App
     public void setScreen(Screen screen){this.screen=screen;}
     public Screen getScreen(Screen screen){return screen;}//Fetchign Screen in App
     public Screen screen;
@@ -43,6 +50,14 @@ public class Menu implements Screen {
     public TextButton playbtn=    new TextButton("PLAY!",skin);
     public void setplaybtn(TextButton playbtn){this.playbtn=playbtn;}//binding playBtn in App
     public DescintrelService service;
+    public void updateByStage(Stage stage){
+        getStage(stage)+setStage(stage);
+    }
+
+    public Stage getStage(Stage stage) {
+        return stage;
+    }
+
     public Stage stage=null;
     public void setStage(Stage stage){this.stage=stage;}//binding stage in App
 public Skin skin;
