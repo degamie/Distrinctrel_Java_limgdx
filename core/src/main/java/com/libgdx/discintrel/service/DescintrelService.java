379c0e4061c8d1ddfdd47613c36faa852c6810c6
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-14 09:09:46 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/service/DescintrelService.java
+++ core/src/main/java/com/libgdx/discintrel/service/DescintrelService.java
@@ -1,4 +1,4 @@
-//WID(12/05/2026)(Sarthak Mittal)(Degamiesign)#1.1s.1.1.1.1.1.1
+//WID(14/05/2026)(Sarthak Mittal)(Degamiesign)#1.1s.1.1.1.1.1.1/.1
 package com.libgdx.discintrel.Service;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -19,6 +19,7 @@ import com.libgdx.discintrel.util.SfxManager;
 //import java.awt.*;
 
 public class DescintrelService extends SfxManager implements ApplicationListener {
+    public void setDelta(float delta){this.delta=delta;}//binidng Delta
     public float delta=0f;
     public void updateBySpeed(int speed){getspeed(speed)+setSpeed(speed)+1;}//updating Speed in App
 
