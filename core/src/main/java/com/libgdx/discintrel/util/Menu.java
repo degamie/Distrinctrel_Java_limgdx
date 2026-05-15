0864494cc5525e3711e1fb0d81da40039e459254
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-15 20:26:40 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/util/Menu.java
+++ core/src/main/java/com/libgdx/discintrel/util/Menu.java
@@ -1,4 +1,4 @@
-//WID(12/05/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1/1.1/1.11.1.1.1.1/1.1/1/1.1/1/1.1
+//WID(15/05/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1/1.1/1.11.1.1.1.1/1.1/1/1.1/1/1.1.1.1.1
 package com.libgdx.discintrel.Menu;
 
 import com.badlogic.gdx.Gdx;
@@ -18,6 +18,17 @@ import com.libgdx.discintrel.Service.DescintrelService;
 import static com.badlogic.gdx.scenes.scene2d.utils.ScissorStack.getViewport;
 
 public class Menu implements Screen {
+    public void updateBySkin(Skin skin){getskin(skin)+setSkin(skin)+1;}//Updating
+    public void setSkin(Skin skin) {this.skin=skin;}
+
+    private Skin getskin(Skin skin) {return skin;}
+
+
+    public void existsByplayBtn(TextButton playbtn){
+        if(playbtn.isChecked())getPlaybtn(playbtn);
+        else getPlaybtn(null);
+    }
+    public void updaatebyPlayBtn(TextButton playbtn){getPlaybtn(playbtn)+setPlaybtn(playbtn)+1;}//Updating playBtn in App
     public void setPlaybtn(TextButton playbtn){this.playbtn=playbtn;}//binding playbtn in App
     public void existsByScreen (Screen screen){
         if(screen!=null)getScreen(screen);
