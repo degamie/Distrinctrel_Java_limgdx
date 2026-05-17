2a721d2bb866ae67b1337cd4cd832de185886566
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-17 06:28:23 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/service/DescintrelService.java
+++ core/src/main/java/com/libgdx/discintrel/service/DescintrelService.java
@@ -1,4 +1,4 @@
-//WID(16/05/2026)(Sarthak Mittal)(Degamiesign)#1.1s.1.1.1.1.1.1/.1/1.1
+//WID(17/05/2026)(Sarthak Mittal)(Degamiesign)#1.1s.1.1.1.1.1.1/.1/1.1.1
 package com.libgdx.discintrel.Service;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -15,11 +15,18 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Array;
 import com.libgdx.discintrel.util.SfxManager;
+
+import java.util.Arrays;
 
 //import java.awt.*;
 
 public class DescintrelService extends SfxManager implements ApplicationListener {
+    public Array<GridPoint2> getValidMoves(BoardState boardState,GridPoint2 start){
+        Array<GridPoint2> moves=new Array<>();
+        int jumpedPieces=0;
+    }
     //GrassHoper's Axial 2d Grid's Coordinates Declare
      public final GridPoint2[] DIRECTIONS={//2d Grid's Directions Declare
         new GridPoint2(-1,0),//Top-Left  cooroinates Declare
