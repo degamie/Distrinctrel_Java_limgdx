67a4e936af2178a18d0c4fef9fba2b0e739d4f43
Degamie_sign <degamiesign@gmail.com>
2026-05-20 20:56:00 +0530

    Binding 2d Grid#1

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(19/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1.1/1/1.1.1/1,1.1.1.1
+//WID(20/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1.1/1/1.1.1/1,1.1.1.1.1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -12,6 +12,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Attributes {
     public GridPoint2[] grid;
+    public void setGrid(GridPoint2[] grid){this.grid=grid;}//binding Grid in App
 //    private int getLevel(int level){return level;}//Fetching vba
     public void existsByCamera(OrthographicCamera camera){if(camera!=null)getcamera(camera);else getcamera(null);}//Checking Camera In App
     public void updateBycamera(OrthographicCamera camera){getCamera(camera)+setCamera(camera)+1;}//Updating Cemera in App
