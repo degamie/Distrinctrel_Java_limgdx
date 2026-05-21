f50cf43ef5d93a43c5693fcbc5b452a40f59eeff
Degamie_sign <degamiesign@gmail.com>
2026-05-21 08:14:08 +0530

    Updating Sfx Manager #1

--- core/src/main/java/com/libgdx/discintrel/util/SfxManager.java
+++ core/src/main/java/com/libgdx/discintrel/util/SfxManager.java
@@ -1,13 +1,16 @@
-//WIP(19/05/2026)(Sarthak Mittal)(DegamieSign)#1.1.1.1.1.1s.1./1/1/1    /1    .1
+//WIP(21/05/2026)(Sarthak Mittal)(DegamieSign)#1.1.1.1.1.1s.1./1/1/1.1
 package com.libgdx.discintrel.util;
 
 //import javafx.scene.media.AudioClip;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.audio.Music;
 import org.w3c.dom.Node;
 
 public class SfxManager {
+    public void updateBysfxManager(SfxManager sfxManager){
+        getSfxManager(sfxManager)+setSfxManager(sfxManager)+1;
+    }
     public SfxManager sfxManager;
     public void existsByNodes(Node nodes){
         if(nodes!=null)getNodea(nodes);
