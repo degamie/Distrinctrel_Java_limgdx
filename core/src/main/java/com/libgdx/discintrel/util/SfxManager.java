127f1ab798309fea93b0307027929c85094bdef7
Degamie_sign <degamiesign@gmail.com>
2026-05-23 07:29:46 +0530

    AiResponseTag_sfx_parsing#

--- core/src/main/java/com/libgdx/discintrel/util/SfxManager.java
+++ core/src/main/java/com/libgdx/discintrel/util/SfxManager.java
@@ -1,4 +1,4 @@
-//WIP(22/05/2026)(Sarthak Mittal)(DegamieSign)#1.1.1.1.1.1s.1./1/1/1.1.1.1
+//WIP(22/05/2026)(Sarthak Mittal)(DegamieSign)#1.1.1.1.1.1s.1./1/1/1.1.1.1.1
 package com.libgdx.discintrel.util;
 
 //import javafx.scene.media.AudioClip;
@@ -8,6 +8,7 @@ package com.libgdx.discintrel.util;
 import org.w3c.dom.Node;
 
 public class SfxManager {
+    String aiRespTage=jsonReader.parse(aiResult).getString("Selected_audio_tag");
     SnapShotArray<Long> activesndids=new SnapShotArray<>();
     public enum gameSound{}
     public void updateBysfxManager(SfxManager sfxManager){
