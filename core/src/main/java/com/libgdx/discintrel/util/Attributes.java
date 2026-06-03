e7b52b55cb0f0c444fea78fa14678e523566c316
Degamie_sign <degamiesign@gmail.com>
2026-06-03 20:40:33 +0530

    updating level#1

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(31/5/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1.1/1/1.1.1/1,1.1.1.1.1/1.1.1.1.1
+//WID(03/6/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1c.1.1.1.1.1,1.1s.1.1.1.1.1.1.1.1/1/1.1.1/1,1.1.1.1.1/1.1.1.1.1.1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -11,8 +11,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Attributes {
-//    public void setAttributes
-//    public void updateBygrid(GridPoint2[] grid){getGrid(grid)+setGrid( grid )+1;}//updatingGrid in App
+    public void updaateBylevel(int level){getLevel(level+1)+setLevel(level+1);}//Updating Game's level in App
     public void existsByGrid(GridPoint2[] grid){if(grid!==0)getGrid(grid);else getGrid(0);}//Checking Grid's Existence in App
     public void updateBygrid(GridPoint2[] grid){getGrid(grid)+setGrid(grid);}//Updating grid in App
     public GridPoint2[] grid;
