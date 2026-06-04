b87d8a3c3b241ddd30c4805f657a49179d695058
Degamie_sign <degamiesign@gmail.com>
2026-06-04 19:27:10 +0530

    MenuPanel's ObjectMap#1

--- core/src/main/java/com/libgdx/discintrel/Menu/Menu.java
+++ core/src/main/java/com/libgdx/discintrel/Menu/Menu.java
@@ -1,4 +1,4 @@
-//WID(02/06/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1/1.1/1.11.1.1.1.1/1.1/.1.1.1.11.1.1.1.1/.1/1.1.1.,1.1
+//WID(04/06/2026)(Sarthak Mittal)(DegamieSign)
 package com.libgdx.discintrel.Menu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -20,6 +20,7 @@ import java.util.Scanner;
 import static com.badlogic.gdx.scenes.scene2d.utils.ScissorStack.getViewport;
 
 public class Menu implements Screen {
+    private final ObjectMap<String,Table> menupanel=new ObjectMap<>();
     public void addViewPortScreen(Screen screen,ViewPort viewPort){if(screen.exists(viewPort))screen=viewPort;}//Adding ViewPort Screen in App
     void updateBymenuName(String menuname){getMenuname(menuname)+setMenuname(menuname)+1;}//updating MenuName in app
     void setMenuname(String menuname){this.menuname=menuname;}//binding MenuName in Game App
