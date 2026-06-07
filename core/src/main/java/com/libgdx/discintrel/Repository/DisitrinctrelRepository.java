6d4a13a0435fdef8580463e57066a87409205120
Degamie_sign <degamiesign@gmail.com>
2026-06-07 13:30:36 +0530

    Grasshopper's animation#1

--- core/src/main/java/com/libgdx/discintrel/Repository/DisitrinctrelRepository.java
+++ core/src/main/java/com/libgdx/discintrel/Repository/DisitrinctrelRepository.java
@@ -1,4 +1,4 @@
-////WID(29/05/2026)(Sarthak Mittal)#1.1.1.1.1.1.1,1.1/1,1.1C.1.1.2.1/1.1.1/1.1.1.1/1.1.1./.11.1.1.1.1
+////WID(07/06/2026)(Sarthak Mittal)(DistrinctrelRepository)
 package com.libgdx.discintrel.Repository;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -18,6 +18,7 @@ import javax.swing.plaf.nimbus.State;
 
 //import static com.libgdx.discintrel.Main.MainApp.entity;
 public interface DisitrinctrelRepository  {
+    public void animmategrasshopper(Animation Grasshopper);
 //    public void getalpha(int alpha);
     public void existsBydelta(int delta);
     public void updateBydelta(int delta);
