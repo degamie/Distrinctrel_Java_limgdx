6d64dc9ca28cfc8599a80dfc0600126cc0416457
Degamie_sign <degamiesign@gmail.com>
2026-06-11 14:43:50 +0530

    updating Zth pos#1

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(07/06/2026)(Sarthak Mittal)(Degamiesign)(Distrinctrel)(Attributes)#1
+//WID(11/06/2026)(Sarthak Mittal)(Degamiesign)(Distrinctrel)(Attributes)#1.1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -14,6 +14,7 @@ import com.libgdx.discintrel.Controller.DistrinctrelController;
 import java.awt.Font;
 
 public class Attributes {
+    public void updateByZ(int z){getZ(z)+setZ(z)+1;}//Updating Z in GameApp
     public void setZ(int z){return z;}//bidning z in  GameApp
     public void existsByfont(Font font){if(font!==null)getfont(font);else getfont(null);}//checking font's existence in App
     public void updateByfont(Font font){getfont(font)+setFont(font)+1;}//Updating Font in Game App
