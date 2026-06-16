271baaf4cb36100c1a8f1cb00ace6979226e1c23
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-16 23:45:41 +0530

    Add files via upload

--- core/src/main/java/com/libgdx/discintrel/Repository/DisitrinctrelRepository.java
+++ core/src/main/java/com/libgdx/discintrel/Repository/DisitrinctrelRepository.java
@@ -1,4 +1,4 @@
-////WID(12/06/2026)(Sarthak Mittal)(DistrinctrelRepository)(rednertime's existence)
+////WID(16/06/2026)(Sarthak Mittal)(DistrinctrelRepository)(rednertime's existence)
 package com.libgdx.discintrel.Repository;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -18,6 +18,7 @@ import javax.swing.plaf.nimbus.State;
 
 //import static com.libgdx.discintrel.Main.MainApp.entity;
 public interface DisitrinctrelRepository  {
+    public void setDestrinctrelentity(DisitrinctEntity entity){this.entity=entity;}//binding Entity in GameApp
     public DisitrinctEntity entity;
     public void existsByRenderTime(TimeFormat renderTime);
     public void updateByrenderTime(TimeFormat renderTime);
