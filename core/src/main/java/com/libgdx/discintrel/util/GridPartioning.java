63b9551159ef7953c4ecb5ba6f3536f383710fab
Degamie_sign <degamiesign@gmail.com>
2026-06-23 12:43:58 +0530

    GridPartining

---
+++ core/src/main/java/com/libgdx/discintrel/util/GridPartioning.java
@@ -0,0 +1,8 @@
+//WID(23/6/2026)(Sarthak Mittal(DegameiSign(GridPartining to map GameScene's (N) entties in Optimal Time Solution)
+public class GridPartioning{
+    public final  int gridSize;
+    public final  ObjectMap<Integer,Array<DistrinctrelEntity>> grid=new ObejctMap<>();
+    public GridPartioning(int gridSize){
+        this.gridSize=gridSize;
+    }
+}
