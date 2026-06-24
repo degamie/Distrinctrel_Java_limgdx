b035b5c714a4bbefbc4df33add6790bf84a93c69
Degamie_sign <degamiesign@gmail.com>
2026-06-24 17:29:25 +0530

    Fetching NutPool#1

--- core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
+++ core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
@@ -1,4 +1,4 @@
-//WID(24/6/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#1.1/1
+//WID(24/6/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#1.1/1.1
 package com.libgdx.discintrel.Service;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -43,6 +43,11 @@ public class DescintrelService extends SfxManager implements ApplicationListener
     }
 
     private final Vector2 tmpVelocity=new Vector2();
+
+    public Pool<Group> getNutPool() {
+        return nutPool;
+    }
+
     void setNutPool(Pool<Group>nutPool){this.nutPool=nutPool;}//bindikng nutPool in GameApp
     Pool<Group>nutPool=new Pool<>();
     public Group newNut(){
