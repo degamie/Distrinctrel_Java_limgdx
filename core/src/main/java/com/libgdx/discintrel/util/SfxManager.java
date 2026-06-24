d514d1586a01f513122b9bf6a183d83653589136
Degamie_sign <degamiesign@gmail.com>
2026-06-24 09:36:16 +0530

    binding ClickSnd#1

--- core/src/main/java/com/libgdx/discintrel/util/SfxManager.java
+++ core/src/main/java/com/libgdx/discintrel/util/SfxManager.java
@@ -1,4 +1,4 @@
-//WIP(19/06/2026)(Sarthak Mittal)(DegamieSign)(SfxManager)#1,1.1
+//WIP(24/06/2026)(Sarthak Mittal)(DegamieSign)(SfxManager)#1,1.1
 package com.libgdx.discintrel.util;
 
 //import javafx.scene.media.AudioClip;
@@ -8,6 +8,7 @@ package com.libgdx.discintrel.util;
 import org.w3c.dom.Node;
 
 public class SfxManager {
+    void setClickedSnd(Sound clicksnd){this.clicksnd=clicksnd;}//binidng CleickSnd IN GameApp
     void setMusicObejctMap(ObjectMap<Music>musicObejctMap){this.musicObejctMap=musicObejctMap;}//binding MusicObjectMap in Game App
     ObejctMap<Music>musicObejctMap=new ObjectMap<>();
     musicObejctMap.add(ambience);
