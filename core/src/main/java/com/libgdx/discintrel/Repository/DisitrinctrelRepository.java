45adddfc3635a3d262c40769b203829be9359329
Degamie_sign <degamiesign@gmail.com>
2026-06-27 07:29:23 +0530

    Binding Stage#1

--- core/src/main/java/com/libgdx/discintrel/Repository/DisitrinctrelRepository.java
+++ core/src/main/java/com/libgdx/discintrel/Repository/DisitrinctrelRepository.java
@@ -1,4 +1,4 @@
-//WID(24/6/2026)(Sarthak Mittal(DegamieSign)(Distrinctrel Repsotiertogt))#1
+//WID(27/6/2026)(Sarthak Mittal(DegamieSign)(Distrinctrel Repsotiertogt))#1
 package com.libgdx.discintrel.Repository;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -18,6 +18,7 @@ import javax.swing.plaf.nimbus.State;
 
 //import static com.libgdx.discintrel.Main.MainApp.entity;
 public interface DisitrinctrelRepository  {
+    void setstage(Stage stage);
     void existsBYstepsCnt(int stpscnt);
     void updateBystepsCnt(int stepscnt);
     void setStpscnt(int stpscnt){this.stpscnt=stpscnt;}//Binding StepsCount in GameApp
