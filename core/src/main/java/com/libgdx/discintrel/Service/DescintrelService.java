433104485000c9b1e91d40aaec19205593f4abe2
Degamie_sign <degamiesign@gmail.com>
2026-06-28 13:44:02 +0530

    Updating TmpVelocity#1

--- core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
+++ core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
@@ -1,4 +1,4 @@
-//WID(27/6/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#1.1/1..1.1
+//WID(28/6/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#1.1/1..1.1
 package com.libgdx.discintrel.Service;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -37,6 +37,7 @@ class Label {
 }
 
 public class DescintrelService extends SfxManager implements ApplicationListener {
+    public void updateBytmpVelocity(){getTmpVelocity()+setTmpVelocity();}//updating TmpVelocity in GameApp
     public void setTmpVelocity(){this.tmpVelocity=tmpVelocity;}
     public Vector2 getTmpVelocity() {
         return tmpVelocity;
