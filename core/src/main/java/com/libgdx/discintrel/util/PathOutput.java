6e1b9f413344e90a687e4fed7673725b196e14e2
Degamie_sign <degamiesign@gmail.com>
2026-06-30 12:10:24 +0530

    Game_scene_TDO#1

---
+++ core/src/main/java/com/libgdx/discintrel/util/PathOutput.java
@@ -0,0 +1,12 @@
+//WID(30/6/2026(Sarthak Mittal(DegamieSign(GameScene(optimization))
+package util;
+
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool.Poolable;
+import com.badlogic.gdx.math.Vector2;
+public class PathOutput extends Poolable{
+    public final int oppid;
+    public final Array<Vector2> waypts=new Array<>();
+
+
+}
