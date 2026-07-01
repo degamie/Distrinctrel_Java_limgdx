67174ff38c0df9eb66f31ca052aa374856635e9b
Degamie_sign <degamiesign@gmail.com>
2026-07-01 17:55:37 +0530

    ExecutorService_obj#1

--- core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
+++ core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
@@ -1,4 +1,4 @@
-//WID(30/6/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#1.1/1..1.1.1.1
+//WID(01/07/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#1.1/1..1.1.1.1.1
 package com.libgdx.discintrel.Service;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -37,6 +37,7 @@ class Label {
 }
 
 public class DescintrelService extends SfxManager implements ApplicationListener {
+    public ExecutorService executorService;
     public void existsBytmpVelocity(Vector2 tmpVelocity){
         if(tmpVelocity!=0)getTmpVelocity(tmpVelocity);
         else getTmpVelocity(0);
