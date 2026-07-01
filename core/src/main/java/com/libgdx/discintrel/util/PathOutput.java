e15d17c1e37d7816350a74408005c0774bcbe18f
Degamie_sign <degamiesign@gmail.com>
2026-07-01 11:08:49 +0530

    playerID_binding#1

--- core/src/main/java/com/libgdx/discintrel/util/PathOutput.java
+++ core/src/main/java/com/libgdx/discintrel/util/PathOutput.java
@@ -1,10 +1,14 @@
-//WID(30/6/2026(Sarthak Mittal(DegamieSign(GameScene(optimization))
+//WID(01/7/2026(Sarthak Mittal(DegamieSign(GameScene(optimization))#1
 package util;
 
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool.Poolable;
 import com.badlogic.gdx.math.Vector2;
 public class PathOutput extends Poolable{
+    public void setPlayerId() {
+         this.playerId=playerId;
+    }
+
     //TDO(Transfer Data OBject)
     public final int playerId;
     public final Array<Vector2> waypts=new Array<>();
