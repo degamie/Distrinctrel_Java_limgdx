a803bce1caabbcbe699922f44bf611e92dd048d7
Degamie_sign <degamiesign@gmail.com>
2026-07-02 18:50:21 +0530

    VectorCache_impl

--- core/src/main/java/com/libgdx/discintrel/util/VectorCache.java
+++ core/src/main/java/com/libgdx/discintrel/util/VectorCache.java
@@ -1,4 +1,6 @@
+//WID(2/7/2026(Sarthak Mittal(VectorCache_Impl)
 public class VectorCache{
+    public void setV2pool(Vector2 v2pool){this.v2pool=v2pool;}
     public final Vector2 v2pool=new Vector2[4];
     public final static indx=0;
 
