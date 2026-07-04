e4ce70d1a605746696c352f2d8cecdff9a90ffb2
Degamie_sign <degamiesign@gmail.com>
2026-07-04 14:07:39 +0530

    Bidning clq#Impl

--- core/src/main/java/com/libgdx/discintrel/util/AsyncWorkerManager.java
+++ core/src/main/java/com/libgdx/discintrel/util/AsyncWorkerManager.java
@@ -1,9 +1,10 @@
-//WID(3/7/2026(Sartthak Mittal(DegamieSign(AsyncWorkerManager))#1.1.1
+//WID(4/7/2026(Sartthak Mittal(DegamieSign(AsyncWorkerManager))#1.1.1
 package util;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import util.PathOutput;
 public class AsyncWorkerManager {
+    public voif setclq(ConcurrentLinkedQueue<PathOutput> clq){this.clq=clq;}//bInidng CLQ in GameApp
     public void setputput(){this.PathOutput=PathOutput;}
     ConcurrentLinkedQueue getConcurrentLinkedQueue(ConcurrentLinkedQueue concurrentLinkedQueue){return concurrentLinkedQueue;}//Fetching concurrentLinkedQueue in GameApp
     public final ConcurrentLinkedQueue<PathOutput> clq=new ConcurrentLinkedQueue();
