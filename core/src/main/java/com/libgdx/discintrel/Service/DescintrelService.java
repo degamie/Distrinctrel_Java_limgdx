95ced71e381b9831e94155b8fa3136892dedfa21
Degamie_sign <degamiesign@gmail.com>
2026-07-04 08:17:09 +0530

    PathCacl_Task#1

--- core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
+++ core/src/main/java/com/libgdx/discintrel/Service/DescintrelService.java
@@ -1,4 +1,4 @@
-//WID(03/07/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#Impl
+//WID(04/07/2026)(Sarthak Mittal)(DeGamieSign)#(DistricntrelService(logic(Optimization(nutpool))#Impl
 package com.libgdx.discintrel.Service;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -39,7 +39,7 @@ class Label {
 public class DescintrelService extends SfxManager implements ApplicationListener {
     public static Pool<PathCalculationTask>calculationTaskPool;
     public void requestAsyncpath(String playerId,final Vector2 start,final Vector2  end){
-        //TBI
+      PathCalculationTask pathCalculationTask;
     }
     public ExecutorService executorService;
     public void existsBytmpVelocity(Vector2 tmpVelocity){
