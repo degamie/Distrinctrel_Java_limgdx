4a7436f6b3d30335dee6803813eb19d6cee49078
Degamie_sign <degamiesign@gmail.com>
2026-07-05 18:13:19 +0530

    Updating pooledParticle

--- core/src/main/java/com/libgdx/discintrel/util/PooledParticleSys.java
+++ core/src/main/java/com/libgdx/discintrel/util/PooledParticleSys.java
@@ -1,4 +1,11 @@
-//WID(4/7/2026)(Sarthak Mittal(DegamieSign(PooledParticleSys)#Impl))
+//WID(5/7/2026)(Sarthak Mittal(DegamieSign(PooledParticleSys)#Impl))(Interview_Related)
 public class PooledParticleSys{
     public final Array<PooledParticle>particle=new Array<>();
+    public void update(float delta){
+        //Updating PooledParticle's arr's  elem's indx from left to right to Optimize in Main Game
+        for(int i=0;i<PooledParticle.length()-1;i++){
+            PooledParticleSys pooledParticleSys=activeParticles.get(i);
+            pooledParticleSys.update(delta);
+        }
+    }
 }
