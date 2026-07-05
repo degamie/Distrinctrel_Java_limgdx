3eb248c9629a1280404c4cab0a6ee1faf3223539
Degamie_sign <degamiesign@gmail.com>
2026-07-05 15:05:35 +0530

    Fetching Vector2#Impl

--- core/src/main/java/com/libgdx/discintrel/util/VectorCache.java
+++ core/src/main/java/com/libgdx/discintrel/util/VectorCache.java
@@ -1,10 +1,14 @@
+//WID(5/7/2026(Sarthak Mittal(VectorCache_Impl)
 import androidx.xr.runtime.math.Vector2;
 
-//WID(4/7/2026(Sarthak Mittal(VectorCache_Impl)s
+
 public class VectorCache{
     public void setindx(int indx){this.indx=indx;}
     public void setV2pool(Vector2 v2pool){this.v2pool=v2pool;}
     public Vector2 v2pool=new Vector2[4];
     public final static int indx=0;
-
+public static Vector2 get(float x,float y){
+    Vector2 v=new v2Pool[indx];
+    v.set(x,y);
+}
 }
