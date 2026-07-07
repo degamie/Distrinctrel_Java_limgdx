022034e27abcded38123776960c7db83acc6481c
Degamie_sign <degamiesign@gmail.com>
2026-07-07 08:43:09 +0530

    ObjectSet#Impl

---
+++ core/src/main/java/com/libgdx/discintrel/util/StatusEffectTracker.java
@@ -0,0 +1,6 @@
+//WID(7.7.2026(Sarthak Mittal(DegamieSign(LIbGDX(ObjectSet#Impl))
+package com.libgdx.discintrel.util;
+
+public class StatusEffectTracker {
+    public final ObjectSet<String> os1=new ObjectSet<>(32);//HashSet's Object  enables  32-Bit Address tracking in GameApp
+}
