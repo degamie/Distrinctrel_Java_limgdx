ccafdfa75477e2c498cfe86fd3d3e3858e7f3475
Degamie_sign <degamiesign@gmail.com>
2026-07-10 19:34:06 +0530

    updating AsyncWorkerManager#Impl

--- core/src/main/java/com/libgdx/discintrel/util/Attributes.java
+++ core/src/main/java/com/libgdx/discintrel/util/Attributes.java
@@ -1,4 +1,4 @@
-//WID(02/07/2026)(Sarthak Mittal)(Degamiesign)(Distrinctrel)(Attributes)#1
+//WID(10s/07/2026)(Sarthak Mittal)(Degamiesign)(Distrinctrel)(Attributes)#1
 package com.libgdx.discintrel.util;
 
 import com.badlogic.gdx.Gdx;
@@ -14,7 +14,11 @@ import com.libgdx.discintrel.Controller.DistrinctrelController;
 import java.awt.Font;
 
 public class Attributes {
-    public void setAsyncworkermanager(AsyncWorkerManager asyncWorkerManager){this.asyncWorkerManager=asyncWorkerManager;}//Binding AsynCWorkerManager in GameApp
+   public void  updateAsyncworkermanager(AsyncWorkerManager asyncWorkerManager){
+        if(asyncWorkerManager!==0)getasyncWorkerManager(asyncWorkerManager);
+        else getasyncWorkerManager(0);
+   }
+       public void setAsyncworkermanager(AsyncWorkerManager asyncWorkerManager){this.asyncWorkerManager=asyncWorkerManager;}//Binding AsynCWorkerManager in GameApp
     public AsyncWorkerManager asyncWorkerManager;
     public void existsByattributes(Attributes attributes){
         if(attributes!=0)getByAttributes(attributes);
