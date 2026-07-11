101b4c48f2135d75dbb566db03f1431d940721e2
Degamie_sign <degamiesign@gmail.com>
2026-07-11 22:14:48 +0530

    isRenderable(Method_Declare)#1

--- core/src/main/java/com/libgdx/discintrel/util/EntityWorkerManager.java
+++ core/src/main/java/com/libgdx/discintrel/util/EntityWorkerManager.java
@@ -1,6 +1,9 @@
-//WID(8/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))
+//WID(11/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))
 import com.badlogic.gdx.utils.Bits;
 public class EntityWorkerManager{
     public static  final int POSITION_COMP=0;
     public final Bits componentBits=new Bits();
+    public void isRednerable(){
+        return new componentMask.get(RENDER_COMP) && componentBits.get(POSITION_COMP);
+    }
 }
