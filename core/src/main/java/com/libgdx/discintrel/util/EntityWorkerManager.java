f8836a94201081fda74b34c6e75e7c16cf9daf50
Degamie_sign <degamiesign@gmail.com>
2026-07-12 13:17:47 +0530

    Binding PositionCompoenent#Impl

--- core/src/main/java/com/libgdx/discintrel/util/EntityWorkerManager.java
+++ core/src/main/java/com/libgdx/discintrel/util/EntityWorkerManager.java
@@ -1,6 +1,7 @@
-//WID(12/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))
+//WID(12/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))#Impl
 import com.badlogic.gdx.utils.Bits;
 public class EntityWorkerManager{
+    public void setPOSITION_COMP(String POSITION_COMP){this.POSITION_COMP=POSITION_COMP;}//binding Position Compoennt in  GameApp
     public static  final int POSITION_COMP=0;
     public final Bits componentBits=new Bits();
     public void addComponent(int compId){
