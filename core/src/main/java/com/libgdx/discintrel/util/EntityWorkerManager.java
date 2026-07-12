1e3f3a27ec055657357b2eabd773a01e779ca95f
Degamie_sign <degamiesign@gmail.com>
2026-07-12 07:58:23 +0530

    Add_Component#Impl

--- core/src/main/java/com/libgdx/discintrel/util/EntityWorkerManager.java
+++ core/src/main/java/com/libgdx/discintrel/util/EntityWorkerManager.java
@@ -1,8 +1,11 @@
-//WID(11/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))
+//WID(12/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))
 import com.badlogic.gdx.utils.Bits;
 public class EntityWorkerManager{
     public static  final int POSITION_COMP=0;
     public final Bits componentBits=new Bits();
+    public void addComponent(int compId){
+        componentBits.set(compId);
+    }
     public void isRednerable(){
         return new componentMask.get(RENDER_COMP) && componentBits.get(POSITION_COMP);
     }
