23cd8c52c7927ac9a527e2f4eeca9dfe88a35173
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-07-24 00:16:49 +0530

    Add files via upload

---
+++ core/src/main/java/com/libgdx/discintrel/util/PathOutput.java
@@ -0,0 +1,43 @@
+//WID(24/7/2026(Sarthak Mittal(DegamieSign(GameScene(optimization))#Impl#1.1.1/1
+package util;
+
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool.Poolable;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+public class PathOutput extends Poolable{
+    public void wayPtsaddon(Array<Vector2> waypts,float x,float y) {
+        for (int i = 0; i < waypts.length; i++) {
+            waypts.add(x);
+            waypts.add(y);}
+            System.out.println(waypts);
+    }
+    public PathOutput getPathOutput(PathOutput pathOutput){
+        this.pathOutput=pathOutput;
+    }
+    @Override
+    public void reset(){
+        playerId-=1;
+        waypts.clear();
+
+    }
+    public void init(float x,float y,float Velx,float VelY){}
+    public void setPathOutput(PathOutput pathOutput){this.pathOutput=pathOutput;}
+    public void requestpath(int playerId,Vector2 start,Vector2 end){
+        PathCalculatedTask  task;
+    }
+    public PathOutput pathOutput;
+    public String getPlayerId(String playerId){return playerId;}//Fetching PlayerId in GameApp
+    void setOutput(PathOutput output){this.output=output;}
+    public PathOutput output;
+    public void setWaypts(Array<Vector2>waypts){this.waypts=waypts;}//binding Waypts in GameApp
+    public void setPlayerId() {
+         this.playerId=playerId;
+    }
+
+    //TDO(Transfer Data OBject)
+    public final int playerId;
+    public final Array<Vector2> waypts=new Array<>();
+
+
+}
