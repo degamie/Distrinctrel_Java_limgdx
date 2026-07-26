dee69aaf2b886bcdca7ac0147e85e7059aa7a086
Degamie_sign <degamiesign@gmail.com>
2026-07-26 19:54:29 +0530

    app_debug_velocity_comp

--- core/src/main/java/com/libgdx/Distrinctrel/util/EntityWorkerManager.java
+++ core/src/main/java/com/libgdx/Distrinctrel/util/EntityWorkerManager.java
@@ -1,6 +1,7 @@
-//WID(12/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))#Impl
+//WID(26/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))#Impl#1
 import com.badlogic.gdx.utils.Bits;
 public class EntityWorkerManager{
+    private final static int VELOCITY_COMP=1;
     public void setPOSITION_COMP(String POSITION_COMP){this.POSITION_COMP=POSITION_COMP;}//binding Position Compoennt in  GameApp
     public static  final int POSITION_COMP=0;
     public final Bits componentBits=new Bits();
