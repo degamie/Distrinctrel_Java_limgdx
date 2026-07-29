03761151b1eb9a5b20498eeb7dd68d321e36585c
Degamie_sign <degamiesign@gmail.com>
2026-07-29 15:33:24 +0530

    ConcurrentAllocFreeMap#Impl

---
+++ core/src/main/java/com/libgdx/Distrinctrel/util/ConcurrentAllocFreeMap.java
@@ -0,0 +1,9 @@
+//WID(29/7/2026)(Sarthak Mittal(DegamieSign))
+public class ConcurrentAllocFreeMap<K,V> {
+    public ObjectMap<K,V> objectMap;
+
+    public ConcurrentAllocFreeMap(int initialCapacity){
+        this.objectMap=objectMap;
+    }
+    
+}
