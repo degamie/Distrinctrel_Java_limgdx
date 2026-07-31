a513540b232358ef573e541930b6559d6a27e629
Degamie_sign <degamiesign@gmail.com>
2026-07-31 10:46:44 +0530

    RENDER_COMP

--- core/src/main/java/com/libgdx/Distrinctrel/util/EntityWorkerManager.java
+++ core/src/main/java/com/libgdx/Distrinctrel/util/EntityWorkerManager.java
@@ -1,6 +1,7 @@
-//WID(26/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))#Impl#1
+//WID(31/7/2026)(Sarthak Mittal(DegamieSign(EntityWorkerManeger))#Impl#1
 import com.badlogic.gdx.utils.Bits;
 public class EntityWorkerManager{
+    public final static int RENDER_COMP=2;
     private final static int VELOCITY_COMP=1;
     public void setPOSITION_COMP(String POSITION_COMP){this.POSITION_COMP=POSITION_COMP;}//binding Position Compoennt in  GameApp
     public static  final int POSITION_COMP=0;
