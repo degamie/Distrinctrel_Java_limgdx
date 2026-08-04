b5d54c1cb496dfad018bd9a79c917773054b53a5
Degamie_sign <degamiesign@gmail.com>
2026-08-05 00:25:29 +0530

    Binding ObjectMap

--- core/src/main/java/com/libgdx/Distrinctrel/util/ConcurrentAllocFreeMap.java
+++ core/src/main/java/com/libgdx/Distrinctrel/util/ConcurrentAllocFreeMap.java
@@ -1,9 +1,12 @@
-//WID(29/7/2026)(Sarthak Mittal(DegamieSign))
+//WID(5/8/2026)(Sarthak Mittal(DegamieSign))#1
 public class ConcurrentAllocFreeMap<K,V> {
+    void setObjectMap(ObjectMap<K,V>objectMap){
+        this.objectMap=objectMap;
+    }
     public ObjectMap<K,V> objectMap;
 
     public ConcurrentAllocFreeMap(int initialCapacity){
         this.objectMap=objectMap;
     }
-    
+
 }
