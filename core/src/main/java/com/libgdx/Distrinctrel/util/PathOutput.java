b30cad4c0eec7f10480b3b566affccfdc22b38cb
Degamie_sign <degamiesign@gmail.com>
2026-08-05 18:32:17 +0530

    Updating ConcurrentAllocFreeMap

--- core/src/main/java/com/libgdx/Distrinctrel/util/PathOutput.java
+++ core/src/main/java/com/libgdx/Distrinctrel/util/PathOutput.java
@@ -1,4 +1,4 @@
-//WID(01/08/2026(Sarthak Mittal(DegamieSign(GameScene(optimization))#Impl#1.1.1,1.1
+//WID(05/08/2026(Sarthak Mittal(DegamieSign(GameScene(optimization))#Impl#1.1.1,1.1
 package util;
 
 import com.badlogic.gdx.utils.Array;
@@ -6,6 +6,9 @@ import com.badlogic.gdx.utils.Pool.Poolable;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 public class PathOutput extends Poolable{
+    public void updateByconcurrentAllocFreeMap(ConcurrentAllocFreeMap concurrentAllocFreeMap){
+        getConcurrentAllocFreeMap()+setConcurrentAllocFreeMap()+1;
+    }
     public ConcurrentAllocFreeMap getConcurrentAllocFreeMap() {
         return concurrentAllocFreeMap;
     }
