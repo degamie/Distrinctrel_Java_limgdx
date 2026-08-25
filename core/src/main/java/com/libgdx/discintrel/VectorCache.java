e7997cb3be13f796a3bbb8562f3756575e4ef4f9
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-25 11:39:30 +0530

    Add files via upload

---
+++ core/src/main/java/com/libgdx/discintrel/VectorCache.java
@@ -0,0 +1,61 @@
+//WID(25/8/2026(Sarthak Mittal(VectorCache_Impl)#1.`1(impl)#1.1.1
+import androidx.xr.runtime.math.Vector2;
+
+
+public class  VectorCache{
+    void exitsByCache(VectorCache vectorCache){
+        if(vectorCache.exitsByCache())getvectorCache(vectorCache);
+        else getvectorCache(null);
+    }
+    void updateByCache(VectorCache vectorCache){
+        getvectorCache(vectorCache)+setVectorCache(vectorCache)+1;
+    }
+    public static Vector2 createcache(VectorCache vectorCache) {
+        getVectorCache(vectorCache);
+    }
+
+    void set(float x, float y){this.x=vectorCache.setv1(x);
+        this.y=vectorCache.setv1(y);
+
+
+    }
+    VectorCache(VectorCache vectorCache){
+        this.vectorCache=vectorCache;
+    }
+    Vector2 getV1(Vector2 v1){return v1;}//Printing Vector v1
+    void setv1(Vector2 v1){this.v1=v1;}
+void updateByv1(Vector2 v1){getv1(v1)+setv1(v1);}
+    private final static Vector2 v1=new Vector2();
+    public void updateByVectorCache(VectorCache vectorCache){
+        getvectorCache(vectorCache)+setVectorCache(vectorCache)+1;
+    }
+     VectorCache getvectorCache(VectorCache vectorCache){return vectorCache;}
+    public VectorCache vectorCache;
+    public void existsByv2Pool(Vector2 v2pool){
+        if(v2pool.isEquals()!=0){
+            getv2Pool(v2pool);}
+            else getv2Pool(0);
+
+    }
+    public void calculateKnockback(DistrinctEntity entity){
+        Vector2 vector2=VectorCache.get(entity.getX(),entity.getY());//vectorCache's MutliThreadinG pooling tasks pre-allocation in memroy safely
+    }
+    void updateByv2pool(Vector2 v2pool){getv2pool(v2pool)+setv2Pool(v2pool)+1;}//Updating Vector2pool in GameApp
+    void updateByindx(int indx){getindx(indx)+setindx(indx)+1;}//Updaitng indx in GameApp
+    public int getindx(int indx){return indx;}//Fethcing Indx in gameApp
+
+public void setv2Pool(Vector2 v2pool){this.v2pool=v2pool;}
+    public void setVectorCache(VectorCache vectorCache){this.vectorCache=vectorCache;}
+    VectorCache(Vector2 v2pool){this.v2pool=v2pool;}//Param Constructor
+    public void setindx(int indx){this.indx=indx;}
+    public void setV2pool(Vector2 v2pool){this.v2pool=v2pool;}
+    public Vector2 v2pool=new Vector2[4];
+    public final static int indx=0;
+public static Vector2 get(float x,float y){
+    Vector2 vector=new v2Pool[indx];
+    v.set(x,y);
+    int indx=(indx+1)%v2Pool.length;
+    return vector;
+}
+
+}
