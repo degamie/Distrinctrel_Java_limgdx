8312fc5b9e5fb0246f15294a55aadafb8d37a482
unknown <degamiesign@gmail.com>
2026-08-28 15:01:17 +0530

    Binding#controller

--- core/src/main/java/com/libgdx/Distrinctrel/Controller/DistrinctrelController.java
+++ core/src/main/java/com/libgdx/Distrinctrel/Controller/DistrinctrelController.java
@@ -1,8 +1,9 @@
-//WID(26/06/2026)(Sarthak Mittal)(DegamieSign)(Controller)s
+//WID(28/06/2026)(Sarthak Mittal)(DegamieSign)(Controller)s1
 package com.libgdx.discintrel.Controller;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.libgdx.discintrel.Service.DescintrelService;
 public class DistrinctrelController  extends DescintrelService {
+    void setController(DistrinctrelController controller){this.controller=controller;}
     public DistrinctrelController controller;
     public DistrinctrelController(DescintrelService descintrelService){
         this.descintrelService=descintrelService;
